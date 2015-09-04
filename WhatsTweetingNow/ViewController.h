c0ec36db35598751ba1341e6a0b82bf0b17af873
Angela Hsu <ayh123@gmail.com>
2015-09-04 11:25:55 -0500

    Initial Commit

---
+++ WhatsTweetingNow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhatsTweetingNow
+//
+//  Created by Angela Hsu on 9/4/15.
+//  Copyright (c) 2015 Optaros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WhatsTweetingNow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WhatsTweetingNow
+//
+//  Created by Angela Hsu on 9/4/15.
+//  Copyright (c) 2015 Optaros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
